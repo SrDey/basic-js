4dc0080d6601d8056b1d66ad763472efc9caade4
SrDey <shaurovdey97@gmail.com>
2023-08-20 00:16:49 +0600

    break

--- array-loop.js
+++ array-loop.js
@@ -1,5 +1,15 @@
-var numbers=[12,23,43,54,57,87,97];
-for(var i=0; i<=numbers.length; i++){
-  var number=numbers[i];
+// var numbers=[12,23,43,54,57,87,97];
+// for(var i=0; i<=numbers.length; i++){
+//   var number=numbers[i];
+//   console.log(number);
+// }
+ var numbers=[12,23,43,43,643,34,23,533];
+ for(var i = 0; i < numbers.length; i++){
+  var number = numbers[i];
+  if(number > 40){
+    // break;
+    continue;
+  }
   console.log(number);
-}
+
+ } 
