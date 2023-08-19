502a4c48654fc0af51f388965596f62ee26686ae
SrDey <shaurovdey97@gmail.com>
2023-08-20 00:39:54 +0600

    reverse

--- array-loop.js
+++ array-loop.js
@@ -3,13 +3,20 @@
 //   var number=numbers[i];
 //   console.log(number);
 // }
- var numbers=[12,23,43,43,643,34,23,533];
- for(var i = 0; i < numbers.length; i++){
-  var number = numbers[i];
-  if(number > 40){
-    // break;
-    continue;
-  }
-  console.log(number);
 
- } 
+//  var numbers=[12,23,43,43,643,34,23,533];
+//  for(var i = 0; i < numbers.length; i++){
+//   var number = numbers[i];
+//   if(number > 40){
+//     // break;
+//     continue;
+//   }
+//   console.log(number);
+
+//  } 
+
+var number=10;
+while(number >= 1){
+  console.log(number);
+  number--;
+}
