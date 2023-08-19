88f54c2071b3680636abb2a91b0c129993b6a563
SrDey <shaurovdey97@gmail.com>
2023-08-19 23:43:28 +0600

    first commit

---
+++ array-loop.js
@@ -0,0 +1,5 @@
+var numbers=[12,23,43,54,57,87,97];
+for(var i=0; i<=numbers.length; i++){
+  var number=numbers[i];
+  console.log(number);
+}
---
+++ variable.js
@@ -0,0 +1,5 @@
+// we need 5 thins to write or creat a variable
+var price = 15; 
+var age = 25;
+var money = 400;
+var weight = 85;
